e97a5c9f3194919178a6bf46b5fe4436cad0607c
IceIT2 <42949053+IceIT2@users.noreply.github.com>
2023-12-05 13:01:11 +0200

    Add files via upload

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonCheck = new System.Windows.Forms.Button();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.lblGuessed = new System.Windows.Forms.Label();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ButtonCheck
+            // 
+            this.ButtonCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonCheck.Location = new System.Drawing.Point(341, 276);
+            this.ButtonCheck.Name = "ButtonCheck";
+            this.ButtonCheck.Size = new System.Drawing.Size(121, 58);
+            this.ButtonCheck.TabIndex = 0;
+            this.ButtonCheck.Text = "Check";
+            this.ButtonCheck.UseVisualStyleBackColor = true;
+            this.ButtonCheck.Click += new System.EventHandler(this.CheckNumber);
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNumber.Location = new System.Drawing.Point(246, 173);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(317, 24);
+            this.textBoxNumber.TabIndex = 1;
+            this.textBoxNumber.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblGuessed
+            // 
+            this.lblGuessed.AutoSize = true;
+            this.lblGuessed.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuessed.Location = new System.Drawing.Point(324, 368);
+            this.lblGuessed.Name = "lblGuessed";
+            this.lblGuessed.Size = new System.Drawing.Size(149, 18);
+            this.lblGuessed.TabIndex = 2;
+            this.lblGuessed.Text = "Ai incercat de 0 ori";
+            this.lblGuessed.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.Location = new System.Drawing.Point(131, 81);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(454, 29);
+            this.lblQuestion.TabIndex = 3;
+            this.lblQuestion.Text = "Ma gandesc la un numar cuprins intre:";
+            this.lblQuestion.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Harlow Solid Italic", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(251, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 46);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ghiceste numarul";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkViolet;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.lblGuessed);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.ButtonCheck);
+            this.Name = "Form1";
+            this.Text = "Number Guessing Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonCheck;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.Label lblGuessed;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+
+        Random randomNumber = new Random();
+
+        int number = 0;
+        int guesses = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            loadQuestions();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckNumber(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBoxNumber.Text);
+
+            guesses += 1;
+            lblGuessed.Text = "Ai incercat de " + guesses + " ori";
+
+            if (i == number)
+            {
+                MessageBox.Show("Felicitari, ai ghicit numarul. Incearca dinnou!");
+                loadQuestions();
+                textBoxNumber.Text = "";
+                guesses = 0;
+                lblGuessed.Text = "Ai incercat de " + guesses + " ori";
+            }
+            else if (i < number)
+            {
+                MessageBox.Show("Mai mult");
+            }
+            else
+            {
+                MessageBox.Show("Mai putin");
+            }
+
+
+        }
+
+        private void loadQuestions()
+        {
+            number = randomNumber.Next(0, 10);
+
+            lblQuestion.Text = "Ma gandesc la un numar cuprins intre: 0 si 10";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
